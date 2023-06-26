073413c83d44b1a3249d2d12372a359c759f8543
Davi Soares <davisfs2110@gmail.com>
2023-06-26 12:00:30 -0300

    Continuação da responsividade(falta o título)

--- script.js
+++ script.js
@@ -1,45 +1,4 @@
-const mobile_menu = document.querySelector(".mobil-menu");
-const nav = document.querySelector("nav");
-const navbar_links_items = document.querySelectorAll(".navbar-links li a");
-const mainSection = document.querySelector(".main-section");
-const header_el = document.querySelector(".header");
-
-let mobiel_menu_open = false;
-
-let options = {
-  root: null,
-  threshold: 0.9,
-  rootMargin: "0px",
-};
-
-const observer = new IntersectionObserver(function (entries, observer) {
-  entries.forEach((entry) => {
-    if (!entry.isIntersecting) {
-      header_el.classList.add("haeder-dark");
-      return;
-    }
-
-    header_el.classList.remove("haeder-dark");
-  });
-}, options);
-
-observer.observe(mainSection);
-
-//mobile menue
-mobile_menu.addEventListener("click", () => {
-  nav.classList.toggle("navbar-links__togle");
-  mobile_menu.classList.toggle("open");
-  mobiel_menu_open = !mobiel_menu_open;
-});
-
-navbar_links_items.forEach((link) => {
-  link.addEventListener("click", () => {
-    nav.classList.toggle("navbar-links__togle");
-    mobile_menu.classList.toggle("open");
-    mobiel_menu_open = false;
-  });
-});
-
+//Script para reescrever a div do title quando a página for redimensionada
 //Efeito em JS na class .title
 var textWrapper = document.querySelector('.title');
 textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
@@ -54,3 +13,24 @@ anime.timeline({loop: false})
     duration: 1200,
     delay: (el, i) => 500 + 30 * i
   });
+
+/* Script para o botão back to top */
+
+var btn = $('#back-top');
+var divDeReferencia = $('#sobre');
+
+$(window).scroll(function() {
+  var posicaoDivReferencia = divDeReferencia.offset().top;
+  var posicaoScroll = $(window).scrollTop();
+
+  if (posicaoScroll > posicaoDivReferencia) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
